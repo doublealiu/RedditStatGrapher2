8a1bf87e84015b939b1a31c540d6879ede78f7a5
quickTwix898 <jacobazhi@gmail.com>
2019-09-27 09:49:04 -0700

    Added more checks

--- src/me/quicktwix898/redditstatgrapher/Main.java
+++ src/me/quicktwix898/redditstatgrapher/Main.java
@@ -11,32 +11,92 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String dashes = "------------------------------------------------------------";
+        final String dashes = "------------------------------------------------------------";
 
         System.out.println(dashes);
 	    System.out.println("RedditStat Grapher");
         System.out.println("Please choose an domain you want to analyze today: ");
         System.out.println("a: r/all");
         System.out.println("s: a specific subreddit");
-        System.out.println("p: a list of posts");
+        System.out.println("p: a post");
         System.out.println(dashes);
 
         char type = reader.readLine().charAt(0);
         while(true) {
-            if (char != ) {
-
+            if (type!='a' && type!='s' && type!='p'){
+                System.out.println("Please choose an option:");
+                type = reader.readLine().charAt(0);
+            } else {
+                break;
             }
         }
 
-        System.out.println(dashes);
-        System.out.println("Please choose an analysis: ");
-        if(type=='a') System.out.println("s: Subreddits in r/all");
-        System.out.println("w: Frequently used words");
-        if(type=='p') System.out.println("u: Upvotes over time");
-        if(type!='p') System.out.println("p: Popularity over time");
-        System.out.println("a: Freqency of activity");
-        System.out.println(dashes);
+        char analysis='0';
+        if(type=='a') {
+            System.out.println(dashes);
+            System.out.println("Please choose an analysis: ");
+            System.out.println("s: subreddits in r/all");
+            System.out.println("w: frequently used words");
+            System.out.println("p: subreddit popularity over time in r/all");
+            System.out.println("a: frequently active times in r/all");
+            System.out.println(dashes);
+            analysis = reader.readLine().charAt(0);
+        } else if(type =='s') {
+            System.out.println(dashes);
+            System.out.println("Please choose an analysis: ");
+            System.out.println("w: frequently used words");
+            System.out.println("a: frequently active times");
+            System.out.println(dashes);
+            analysis = reader.readLine().charAt(0);
+        } else if(type=='p') {
+            System.out.println("Please choose an analysis: ");
+            System.out.println("w: frequently used words");
+            System.out.println("u: upvotes over time");
+            System.out.println(dashes);
+            analysis = reader.readLine().charAt(0);
+        }
 
-        char analysis = reader.readLine().charAt(0);
+        while(true) {
+            if (analysis!='s' && analysis!='w' && analysis!='u' && analysis!='u' && analysis!='p' && analysis!='a'){
+                System.out.println("Please choose an option:");
+                analysis = reader.readLine().charAt(0);
+            } else break; }
+
+        if(type=='a' && analysis == 's') {
+            System.out.println(dashes);
+            System.out.println("Please choose an amount of posts: ");
+            int posts = Integer.parseInt(reader.readLine());
+            //map = getAllSubreddits(posts);
+            System.out.println("Please choose a graph type: ");
+            System.out.println("b: bar chart");
+            System.out.println("p: pie chart");
+            char chart = reader.readLine().charAt(0);
+            while(true) {
+                if(analysis!='b' && analysis!='p') System.out.println("Please choose an option: ");
+                else break; }
+            System.out.println("Please specify path to save graph (must end in png): ");
+            String filename = reader.readLine();
+            System.out.println(dashes);
+            System.out.println("Generating graph...");
+            // if(chart == 'p') graphPie(map, filename);
+            // else if(chart == 'b') graphBar(map, filename);
+        }
+        else if(type == 'a' && analysis == 'w') {
+            System.out.println(dashes);
+            System.out.println("Please choose an amount of posts: ");
+            int posts = Integer.parseInt(reader.readLine());
+            // map = getAllParsedWords(posts);
+            System.out.println(dashes);
+            System.out.println("Please choose a graph type: ");
+            System.out.println("w: word cloud");
+            System.out.println("b: bar chart");
+            System.out.println("p: pie chart");
+            char chart = reader.readLine().charAt(0);
+            while(true) {
+                if(chart!='w' && chart !='b' && chart!='p') System.out.println("Please choose an option: ");
+                else break; }
+            System.out.println("Please specify path to save graph (must end in png): ");
+            System.out.println(dashes);
+        }
     }
 }
