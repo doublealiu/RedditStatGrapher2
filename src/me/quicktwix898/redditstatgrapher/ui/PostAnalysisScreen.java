23e385e2940d22973a0bf5e6be20d39295528f67
quickTwix898 <jacobazhi@gmail.com>
2019-10-01 09:53:43 -0700

    Complemted post analysis screen class

--- src/me/quicktwix898/redditstatgrapher/ui/PostAnalysisScreen.java
+++ src/me/quicktwix898/redditstatgrapher/ui/PostAnalysisScreen.java
@@ -1,4 +1,47 @@
 package me.quicktwix898.redditstatgrapher.ui;
 
-public class PostAnalysisScreen {
+public class PostAnalysisScreen implements TerminalScreen {
+    final static PostAnalysisScreen INSTANCE = new PostAnalysisScreen();
+    final String message = DASHED_LINE + "\n" +
+            "Please choose an analysis:\n" +
+            "w: frequently used words\n" +
+            "u: upvotes over time\n" +
+            DASHED_LINE + "\n";
+
+    @Override
+    public String getDisplay() { return message; }
+
+    public static TerminalScreen getInsstance() { return INSTANCE; }
+
+    public PostAnalysisScreenChoice getChoice(char c) {
+        return PostAnalysisScreenChoice.get(c);
+    }
+
+    public enum PostAnalysisScreenChoice implements TerminalChoice{
+        words('w', ),
+        upvotes('u', );
+
+        char character;
+        TerminalScreen screen;
+
+        PostAnalysisScreenChoice(char character, TerminalScreen screen) {
+            this.character = character;
+            this.screen = screen;
+        }
+
+        @Override
+        public TerminalScreen getScreen{ return screen; }
+
+        @Override
+        public char getCharacter{ return character; }
+
+        public static PostAnalysisScreenChoice get(char c) {
+            for(PostAnalysisScreenChoice choice : value()) {
+                if (choice.character == c) {
+                    return choice;
+                }
+            }
+            return null;
+        }
+    }
 }
