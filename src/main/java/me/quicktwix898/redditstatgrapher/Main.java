8df95157b54b60e0816151aac5aa5b6eddeed1aa
doublealiu <doublealiu@gmail.com>
2019-11-01 09:26:05 -0700

    aaronm push2

--- src/main/java/me/quicktwix898/redditstatgrapher/Main.java
+++ src/main/java/me/quicktwix898/redditstatgrapher/Main.java
@@ -8,7 +8,7 @@ import me.quicktwix898.redditstatgrapher.analysis.AnalysisAction;
 public class Main {
 
     public static void main(String[] args){
-        HikariDataSource ds = null; //SQLUIHandler.start();
+        HikariDataSource ds = SQLUIHandler.start();
         List<Object> choices = UIHandler.getInstance().start();
         AnalysisAction analysis = new ChoiceReader(choices, ds).get();
         analysis.graph();
