0aa88ab3826dc2f2e6796c4c34651cc30ec4a5a8
doublealiu <doublealiu@gmail.com>
2019-11-04 09:16:23 -0800

    Updated igtignore file

--- src/main/java/me/quicktwix898/redditstatgrapher/Test.java
+++ src/main/java/me/quicktwix898/redditstatgrapher/Test.java
@@ -1,7 +1,12 @@
 package me.quicktwix898.redditstatgrapher;
 
+import com.zaxxer.hikari.HikariConfig;
+
 public class Test {
     public static void main(String[] args) {
-
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://www.db4free.net/redditstat/");
+        config.setUsername("wsmachine");
+        config.set
     }
 }
